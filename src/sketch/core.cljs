a1242a7fa4930f19271ed6b51477d86896f39c5f
Gabriel Andretta <gabriel.andretta@gmail.com>
2020-04-10 15:55:21 -0300

    Add example sketch from get started page

--- src/sketch/core.cljs
+++ src/sketch/core.cljs
@@ -1,1 +1,11 @@
 (ns sketch.core)
+
+(defn setup []
+  (js/createCanvas 640 480))
+
+(defn draw []
+  (js/fill (if js/mouseIsPressed 0 255))
+  (js/ellipse js/mouseX js/mouseY 80 80))
+
+(set! (.-setup js/window) setup)
+(set! (.-draw js/window) draw)
