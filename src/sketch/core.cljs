dae0799b205d63ce30a9213b22f0f391195db5ef
Gabriel Andretta <gabriel.andretta@gmail.com>
2020-04-10 16:40:06 -0300

    Make defsketch body look like extend-protocol

--- src/sketch/core.cljs
+++ src/sketch/core.cljs
@@ -2,25 +2,27 @@
   (:require [sketch.p5 :as p5]))
 
 (p5/defsketch example
-  [["setup" (fn [p]
-              (.createCanvas p 640 480))]
-   ["draw" (fn [p]
-             (.fill p (if (.-mouseIsPressed p) 0 255))
-             (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))]])
+  (setup [p]
+    (.createCanvas p 640 480))
+  (draw [p]
+    (.fill p (if (.-mouseIsPressed p) 0 255))
+    (.ellipse p (.-mouseX p) (.-mouseY p) 80 80)))
+
 (comment
   (macroexpand-1 '(p5/defsketch example
-                    [["setup" (fn [p]
-                                (.createCanvas p 640 480))]
-                     ["draw" (fn [p]
-                               (.fill p (if (.-mouseIsPressed p) 0 255))
-                               (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))]]))
+                    (setup [p]
+                      (.createCanvas p 640 480))
+                    (draw [p]
+                      (.fill p (if (.-mouseIsPressed p) 0 255))
+                      (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))))
+
   ;; yields
 
   (do (sketch.p5/ensure-parent "example")
       (def example (sketch.p5/instance
-                    [["setup" (fn [p]
+                    [["setup" (clojure.core/fn [p]
                                 (.createCanvas p 640 480))]
-                     ["draw" (fn [p]
+                     ["draw" (clojure.core/fn [p]
                                (.fill p (if (.-mouseIsPressed p) 0 255))
                                (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))]]
                     "example"))))
--- src/sketch/p5.clj
+++ src/sketch/p5.clj
@@ -1,6 +1,10 @@
 (ns sketch.p5)
 
-(defmacro defsketch [name methods-spec]
-  (let [parent-id (str name)]
+(defn- method-form->method-spec [[name & args-and-body]]
+  [(str name) (conj args-and-body 'clojure.core/fn)])
+
+(defmacro defsketch [name & method-forms]
+  (let [parent-id (str name)
+        methods-spec (mapv method-form->method-spec method-forms)]
     `(do (ensure-parent ~parent-id)
          (def ~name (instance ~methods-spec ~parent-id)))))
