aa09f425a6b242a1a8e90876b09895b634ed24d1
Gabriel Andretta <gabriel.andretta@gmail.com>
2020-04-10 16:22:05 -0300

    Inline setup and draw functions

--- src/sketch/core.cljs
+++ src/sketch/core.cljs
@@ -1,16 +1,15 @@
 (ns sketch.core
   (:require [sketch.p5 :as p5]))
 
-(defn setup [p]
-  (.createCanvas p 640 480))
-
-(defn draw [p]
-  (.fill p (if (.-mouseIsPressed p) 0 255))
-  (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))
-
 (def parent-id  "example")
 
 (p5/ensure-parent parent-id)
 
 (def example
-  (p5/instance [["setup" setup] ["draw" draw]] parent-id))
+  (p5/instance
+   [["setup" (fn [p]
+               (.createCanvas p 640 480))]
+    ["draw" (fn [p]
+              (.fill p (if (.-mouseIsPressed p) 0 255))
+              (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))]]
+   parent-id))
