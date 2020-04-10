2c0b634c96fa29ce0bd3b33f3b8c99a8b137299b
Gabriel Andretta <gabriel.andretta@gmail.com>
2020-04-10 16:07:50 -0300

    Convert to instance mode

--- src/sketch/core.cljs
+++ src/sketch/core.cljs
@@ -1,11 +1,21 @@
-(ns sketch.core)
+(ns sketch.core
+  (:require [goog.dom :as d]))
 
-(defn setup []
-  (js/createCanvas 640 480))
+(defn setup [p]
+  (.createCanvas p 640 480))
 
-(defn draw []
-  (js/fill (if js/mouseIsPressed 0 255))
-  (js/ellipse js/mouseX js/mouseY 80 80))
+(defn draw [p]
+  (.fill p (if (.-mouseIsPressed p) 0 255))
+  (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))
 
-(set! (.-setup js/window) setup)
-(set! (.-draw js/window) draw)
+(def parent-id  "example")
+
+(when-not (d/getElement parent-id)
+  (d/append js/document.body (d/createDom "div" #js {:id parent-id})))
+
+(def example
+  (new js/p5
+       (fn [p]
+         (set! (.-setup p) (fn [] (setup p)))
+         (set! (.-draw p) (fn [] (draw p))))
+       parent-id))
